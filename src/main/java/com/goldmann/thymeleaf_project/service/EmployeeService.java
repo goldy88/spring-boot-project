3644489cc68c6e43e27db40c637b8bfc9a04bb2f
unknown <goldmann.daniel.88@gmail.com>
2021-11-17 20:06:11 +0100

    2. pridani stránkování

--- src/main/java/com/goldmann/thymeleaf_project/service/EmployeeService.java
+++ src/main/java/com/goldmann/thymeleaf_project/service/EmployeeService.java
@@ -1,6 +1,7 @@
 package com.goldmann.thymeleaf_project.service;
 
 import com.goldmann.thymeleaf_project.model.Employee;
+import org.springframework.data.domain.Page;
 
 import java.util.List;
 
@@ -9,5 +10,7 @@ public interface EmployeeService {
     void saveEmployee(Employee employee);
     Employee getEmployeeById(long id);
     void deleteEmployeeByID(long id);
+    Page < Employee > findPaginated(int pageNo, int pageSize);
 
 }
+
--- src/main/java/com/goldmann/thymeleaf_project/service/EmployeeServiceImpl.java
+++ src/main/java/com/goldmann/thymeleaf_project/service/EmployeeServiceImpl.java
@@ -3,6 +3,9 @@ package com.goldmann.thymeleaf_project.service;
 import com.goldmann.thymeleaf_project.model.Employee;
 import com.goldmann.thymeleaf_project.repository.EmployeeRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -40,5 +43,11 @@ public class EmployeeServiceImpl implements EmployeeService {
     @Override
     public void deleteEmployeeByID(long id) {
         this.employeeRepository.deleteById(id);
+    }
+
+    @Override
+    public Page<Employee> findPaginated(int pageNo, int pageSize) {
+        Pageable pageable = PageRequest.of(pageNo - 1, pageSize);
+        return this.employeeRepository.findAll(pageable);
     }
 }
